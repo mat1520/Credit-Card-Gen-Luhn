ff15c7ef7875818a066b3ce02dae0f9312f1ec8f
mat1520 <azaazay09@gmail.com>
2025-04-25 19:24:46 -0500

    fix: update vercel and vite config for proper static file serving

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html')
+                main: resolve(__dirname, 'index.html'),
+                binLookup: resolve(__dirname, 'bin-lookup.html')
             }
         }
     },
